23506a76209e79c0b7763c7beb92e81418985784
Riedonefadillah <rridwanfadillah39@gmail.com>
2025-09-05 19:29:03 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ menuIcon.onclick = () =>{
     menuIcon.classList.toggle('.fas.fa-bars')
     navbar.classList.toggle('active')
 }
+
+const audio = document.getElementById('backsound');
+const btn = document.getElementById('soundControl');
+const icon = btn.querySelector('.fas.fa-volume-mute');
+
+btn.addEventListener('click',() => {
+    audio.muted = !audio.muted;
+    if(audio.muted){
+        icon.classList.remove('fa-volume-up');
+        icon.classList.add('fa-volume-mute');
+    } else{
+        icon.classList.remove('fa-volume-mute');
+        icon.classList.add('fa-volume-up');
+    }
+})
+
+// const music = document.getElementById('backsound');
+// const startMusic = () => {
+//     music.play();
+//     document.removeEventListener('click', startMusic);
+// };
+// document.addEventListener('click', startMusic)
